43cf7447db72979e1e7720f6eff9c2d580ec526e
Sushmit94 <sushmitshrm@gmail.com>
2025-08-07 14:28:17 +0530

    Update twitter.sol

--- twitter.sol
+++ twitter.sol
@@ -1,10 +1,16 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Twitter{
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    uint16 MAX_TWEET_LENGTH = 280;
+contract Twitter is Ownable {
 
-    struct Tweet{
+
+
+     constructor(address initialOwner) Ownable(initialOwner) {}
+    uint16 public MAX_TWEET_LENGTH = 280;
+
+    struct Tweet {
         uint256 id;
         address author;
         string content;
@@ -12,58 +18,59 @@ contract Twitter{
         uint256 likes;
     }
 
-    mapping (address => Tweet[]) public tweets;
-    address public owner;
+    mapping(address => Tweet[]) public tweets;
 
-    constructor(){
-        owner = msg.sender;
-    }
+    event TweetCreated(uint256 id, address author, string content, uint256 timestamp);
+    event TweetLiked(address liker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
+    event TweetUnliked(address unliker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
 
-    modifier onlyOwner(){
-        require(msg.sender == owner,"You are not the owner");
-        _;
-    }
-
-    event TweetCreated(uint256 id,address author,string content,uint256 timestamp);
-    event TweetLiked(address liker,address tweetAuthor, uint256 tweetId,uint256 newLikeCount);
-     event TweetUnliked(address unliker,address tweetAuthor, uint256 tweetId,uint256 newLikeCount);
-
-    function changeTweetLenth(uint16 newTweetLength) public onlyOwner{
+    function changeTweetLenth(uint16 newTweetLength) public onlyOwner {
         MAX_TWEET_LENGTH = newTweetLength;
     }
 
-    function createTweet(string memory _tweet) public  {
-        require(bytes(_tweet).length <=MAX_TWEET_LENGTH,"tweet is long");
-       Tweet memory newTweet = Tweet({
-        id:tweets[msg.sender].length,
-        author: msg.sender,
-        content:_tweet,
-        timestamp: block.timestamp,
-        likes: 0
-       });
-       tweets[msg.sender].push(newTweet);
-       emit TweetCreated(newTweet.id,newTweet.author,newTweet.content,newTweet.timestamp);
+    function getTotalLikes(address _author) external view returns (uint256) {
+        uint256 totalLikes = 0;
+        for (uint256 i = 0; i < tweets[_author].length; i++) {
+            totalLikes += tweets[_author][i].likes;
+        }
+        return totalLikes;
+    }
+
+    function createTweet(string memory _tweet) public {
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long");
+
+        Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamp);
     }
 
     function likeTweet(address author, uint256 id) public {
-        require(tweets[author][id].id == id,"Tweet does not exist");
+        require(id < tweets[author].length, "Tweet does not exist");
+
         tweets[author][id].likes++;
-        emit TweetLiked(msg.sender,author,id,tweets[author][id].likes);
+        emit TweetLiked(msg.sender, author, id, tweets[author][id].likes);
     }
 
-    function unliketweets(address author , uint256 id) public{
-          require(tweets[author][id].id == id,"Tweet does not exist");
-          require(tweets[author][id].likes>0,"This tweet has 0 likes ,cannot dislike");
-          tweets[author][id].likes++;
-          emit TweetUnliked(msg.sender,author,id,tweets[author][id].likes);
+    function unlikeTweet(address author, uint256 id) public {
+        require(id < tweets[author].length, "Tweet does not exist");
+        require(tweets[author][id].likes > 0, "This tweet has 0 likes, cannot unlike");
+
+        tweets[author][id].likes--;
+        emit TweetUnliked(msg.sender, author, id, tweets[author][id].likes);
     }
 
-    function getTweet(address _owner, uint256 _i) public view returns(Tweet memory){
+    function getTweet(address _owner, uint256 _i) public view returns (Tweet memory) {
         return tweets[_owner][_i];
     }
 
-    function getAllTweets(address _owner) public view returns(Tweet[] memory){
+    function getAllTweets(address _owner) public view returns (Tweet[] memory) {
         return tweets[_owner];
     }
-
 }
